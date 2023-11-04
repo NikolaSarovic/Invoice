2aa8a2abf751a33ba80f6f3d1454317f75f45c5d
nikola225 <sarovic_n@yahoo.com>
2023-11-04 22:48:04 +0100

    get invoices and invoice by id .Delete invoice  and delete item

---
+++ Backend/fakture/fakture/Controllers/Products/ProductsDtos/ArtikalDto.cs
@@ -0,0 +1,23 @@
+﻿using fakture.Models.Domain;
+
+namespace fakture.Controllers.Products.ProductsDtos
+{
+    public class ArtikalDto
+    {
+        public int ArtikalId { get; set; }
+        public string NazivArtikla { get; set; }
+        public int Kolicina { get; set; }
+        public float Cijena { get; set; }
+        public float PostoRabata { get; set; }
+
+        public ArtikalDto(Artikal dbArtikal)
+        {
+            ArtikalId = dbArtikal.ArtikalId;
+            NazivArtikla = dbArtikal.NazivArtikla;
+            Kolicina = dbArtikal.Kolicina;
+            Cijena = dbArtikal.Cijena;
+            PostoRabata = dbArtikal.PostoRabata;
+
+        }
+    }
+}
---
+++ Backend/fakture/fakture/Controllers/Products/ProductsRepository/IProductsRepository.cs
@@ -0,0 +1,12 @@
+﻿using fakture.Controllers.Products.ProductsDtos;
+
+namespace fakture.Controllers.Products.ProductsRepository
+{
+    public interface IProductsRepository
+    {
+        public Task<IEnumerable<FakturaDto>> GetFakture(string userId);
+        public Task<FakturaDto> GetFakturaById(int fakturaId);
+        public Task<ArtikalDto> DeleteArtikal(int artikalId);
+        public Task<FakturaDto> DeleteFaktura(int fakturaId);
+    }
+}
